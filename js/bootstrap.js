db2c911abeb8619318fc94e597a7f708d8bda476
Mike Pezzi <mike@thespiral.ca>
2012-02-10 21:31:01 -0500

    added alert and collapse scripts

--- js/bootstrap.js
+++ js/bootstrap.js
@@ -1,3 +1,15 @@
-//$(function(){
-//  $('.collapse').collapse();
-//});
+(function($){
+  $(function(){
+    if ( $('body.toolbar .navbar-fixed-top').length > 0 ) {
+      $('body').css('padding-top', '105px');
+    }
+
+    if ( $('body.toolbar-drawer .navbar-fixed-top').length > 0 ) {
+      $('body').css('padding-top', '105px');
+    }
+
+    $('.collapse').collapse();
+    $('.alert-message').alert();
+
+  });
+})($jq171);
