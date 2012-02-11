41f4ca12aa659da83af58b9af25bcbbf678fb4be
Mike Pezzi <mike@thespiral.ca>
2012-02-10 21:33:17 -0500

    added html5 shiv and jquery 1.7.1 with noConflict

--- templates/html.tpl.php
+++ templates/html.tpl.php
@@ -4,12 +4,22 @@
   <meta charset="utf-8">
   <?php print $head; ?>
   <title><?php print $head_title; ?></title>
+
+  <!--[if lt IE 9]>
+    <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+  <![endif]-->
+
   <?php print $styles; ?>
-  <?php print $scripts; ?>
 </head>
 <body class="<?php print $classes; ?>" <?php print $attributes; ?>>
   <?php print $page_top; ?>
   <?php print $page; ?>
   <?php print $page_bottom; ?>
+
+  <!-- Include jQuery 1.7.1 with noConflict() -->
+  <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
+  <script type="text/javascript">var $jq171 = jQuery.noConflict();</script>
+  <?php print $scripts; ?>
+
 </body>
 </html>
