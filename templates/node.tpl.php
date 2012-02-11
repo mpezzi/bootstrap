cc5cb097757852b813d55b808df820d2f34caba3
Mike Pezzi <mike@thespiral.ca>
2012-02-10 21:32:46 -0500

    changed node title and submitted elements

--- templates/node.tpl.php
+++ templates/node.tpl.php
@@ -2,13 +2,13 @@
   <?php print $user_picture; ?>
 
   <?php if ( !$page ): ?>
-    <h3><a href="<?php print $node_url; ?>"><?php print $title; ?></a></h3>
+    <h2><a href="<?php print $node_url; ?>"><?php print $title; ?></a></h2>
   <?php endif; ?>
 
   <?php if ( $display_submitted ): ?>
-    <div class="submitted">
-      <?php print $submitted; ?>
-    </div>
+    <p class="submitted">
+      <small><?php print $submitted; ?></small>
+    </p>
   <?php endif; ?>
 
   <div class="content"<?php print $content_attributes; ?>>
