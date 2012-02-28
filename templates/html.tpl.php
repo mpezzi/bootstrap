d4c11ed7714c31ce20cfe197a7a242fe176e31ce
Mike Pezzi <mike@thespiral.ca>
2012-02-27 23:26:04 -0500

    added viewport meta tag

--- templates/html.tpl.php
+++ templates/html.tpl.php
@@ -4,6 +4,7 @@
   <meta charset="utf-8">
   <?php print $head; ?>
   <title><?php print $head_title; ?></title>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
   <!--[if lt IE 9]>
     <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
